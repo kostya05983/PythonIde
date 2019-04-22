5707d16c75e25e914ddb1415e8c44f0fd95ad866
kostya05983 <kostya05983@mail.ru>
2019-04-22 13:02:58 +0700

    [FIX] fix states

--- core/src/main/kotlin/scanner/Token.kt
+++ core/src/main/kotlin/scanner/Token.kt
@@ -1,3 +1,8 @@
 package scanner
 
-class Token(val token: Tokens, val value: String)
+class Token(val token: Tokens, val value: String) {
+    override fun equals(other: Any?): Boolean {
+        val otherToken = other as Token
+        return token == otherToken.token && value == otherToken.value
+    }
+}
--- core/src/test/kotlin/scanner/ariphmeticScanAutomat/ArithmeticScannerTest.kt
+++ core/src/test/kotlin/scanner/ariphmeticScanAutomat/ArithmeticScannerTest.kt
@@ -1,6 +1,8 @@
 package scanner.ariphmeticScanAutomat
 
 import org.junit.jupiter.api.Assertions.*
+import scanner.Token
+import scanner.Tokens
 
 internal class ArithmeticScannerTest {
 
@@ -13,6 +15,28 @@ internal class ArithmeticScannerTest {
     fun scan() {
         val arithmeticScanner = ArithmeticScanner()
         arithmeticScanner.scan(test)
-        println(arithmeticScanner.currenState.tokensArray)
+        val tokens = arithmeticScanner.currenState.tokensArray
+        assertEquals(Token(Tokens.IDENTIFIER, "a"), tokens[0])
+        assertEquals(Token(Tokens.EQUAL, Tokens.EQUAL.literal), tokens[1])
+        assertEquals(Token(Tokens.IDENTIFIER, "5"), tokens[2])
+        assertEquals(Token(Tokens.AND, Tokens.AND.literal), tokens[3])
+        assertEquals(Token(Tokens.IDENTIFIER, "test"), tokens[4])
+        assertEquals(Token(Tokens.EQUAL, Tokens.EQUAL.literal), tokens[5])
+        assertEquals(Token(Tokens.IDENTIFIER, "7"), tokens[6])
+        assertEquals(Token(Tokens.SHIFT_LEFT, Tokens.SHIFT_LEFT.literal), tokens[7])
+        assertEquals(Token(Tokens.IDENTIFIER, "2"), tokens[8])
+        assertEquals(Token(Tokens.BINARY_AND, Tokens.BINARY_AND.literal), tokens[9])
+        assertEquals(Token(Tokens.IDENTIFIER, "238"), tokens[10])
+        assertEquals(Token(Tokens.AND, Tokens.AND.literal), tokens[11])
+        assertEquals(Token(Tokens.IDENTIFIER, "teta"), tokens[12])
+        assertEquals(Token(Tokens.NOT_EQUAL, Tokens.NOT_EQUAL.literal), tokens[13])
+        assertEquals(Token(Tokens.INVERSE, Tokens.INVERSE.literal), tokens[14])
+        assertEquals(Token(Tokens.LEFT_BRACKET, Tokens.LEFT_BRACKET.literal), tokens[15])
+        assertEquals(Token(Tokens.IDENTIFIER, "25"), tokens[16])
+        assertEquals(Token(Tokens.SHIFT_RIGHT, Tokens.SHIFT_RIGHT.literal), tokens[17])
+        assertEquals(Token(Tokens.IDENTIFIER, "34"), tokens[18])
+        assertEquals(Token(Tokens.SHIFT_LEFT, Tokens.SHIFT_LEFT.literal), tokens[19])
+        assertEquals(Token(Tokens.IDENTIFIER, "32"), tokens[20])
+        assertEquals(Token(Tokens.RIGHT_BRACKET, Tokens.RIGHT_BRACKET.literal), tokens[21])
     }
 }
